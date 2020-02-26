620f942aa7c787b97db0421952080f842aa12969
Webskey <jolo_92@wp.pl>
2020-02-26 15:37:18 +0100

    another one

--- main.js
+++ main.js
@@ -17,26 +17,45 @@ function playVid(videoId) {
 }
 
 function displayVids(items) {
+  console.log(items);
+  const foundContainer = document.createElement("div");
+  //foundContainer.classList.add("found-countainer");
+  foundContainer.id = "found-container";
+  document.body.appendChild(foundContainer);
   items.forEach(item => {
-    const container = document.createElement("div");
-    const image = document.createElement("img");
-    image.src = item.snippet.thumbnails.high.url;
-    container.appendChild(image);
+    // found element
+    const foundElement = document.createElement("div");
+    foundElement.classList.add("found-element");
+    // title
     const title = document.createElement("p");
+    title.classList.add("found-element-title")
     title.innerHTML = item.snippet.title;
-    container.appendChild(title);
-    container.addEventListener("click", () => playVid(item.id.videoId));
-    document.body.appendChild(container);
+    foundElement.appendChild(title);
+    // image
+    const image = document.createElement("img");
+    image.classList.add("found-element-image")
+    image.src = item.snippet.thumbnails.high.url;
+    foundElement.appendChild(image);
+    
+    foundElement.addEventListener("click", () => playVid(item.id.videoId));
+    foundContainer.appendChild(foundElement);
   });
 }
 
 function apiSearch(search) {
   fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${search.split(" ").join()}&key=AIzaSyBqFnd9RMee7nkMTaB5obHflnz-IDx3ubs`
+    //`https://www.googleapis.com/youtube/v3/search?part=contentDetails&q=${search.split(" ").join()}&key=AIzaSyBqFnd9RMee7nkMTaB5obHflnz-IDx3ubs`
+     `https://www.googleapis.com/youtube/v3/search?key=AIzaSyBqFnd9RMee7nkMTaB5obHflnz-IDx3ubs&channelId=UC6Wej54Yh6uCABltYSWKDNw&part=snippet,id&order=date&maxResults=40`
   )
     .then(resp => resp.json())
+    // Another API req to get video duration timers
+    // .then(resp => fetch(`https://www.googleapis.com/youtube/v3/videos?id=${resp.items.map(item => item.id.videoId).join(',')}&part=snippet,contentDetails&key=AIzaSyBqFnd9RMee7nkMTaB5obHflnz-IDx3ubs`))
+    // .then(resp => resp.json())
     .then(resp => displayVids(resp.items));
 }
+
+
+
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search");
 searchButton.addEventListener("submit", (event) => {
